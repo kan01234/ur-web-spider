24ae427604a28f3910203f93da2aee0409dd39be
kan01234 <safghjkl@gmail.com>
2022-11-25 17:10:35 +0900

    add try catch

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 
+import traceback
 import requests
 import json
 from datetime import datetime
@@ -27,7 +28,6 @@ requestBuilder = RequestBuilder(isDev)
 # open file write stream
 jsonRowCount = -1
 
-
 with open(OUTPUT_FILE_NAME + ".json", "w") as jsonFile, open(OUTPUT_FILE_NAME + ".csv", "w") as csvFile:
   showHeader = True
   hasNextPage = True
@@ -38,31 +38,34 @@ with open(OUTPUT_FILE_NAME + ".json", "w") as jsonFile, open(OUTPUT_FILE_NAME + 
       for skcs in skcsStr.split(","):
         print("fetching", block, tdfk, skcs)
         page=-1
-        while hasNextPage:
-          page+=1
-          bukkens = requestBuilder.postBukkenResult(block=block, tdfk=tdfk, skcs=skcs, page=page)
-          if (bukkens == None or len(bukkens) == 0 or int(bukkens[0]["roomCount"]) == 0):
-            break
-          for bukkenJson in bukkens:
-            jsonRowCount += 1
-            # write to json file
-            if (jsonRowCount == 0):
-              jsonFile.write("\n  " + json.dumps(bukkenJson))
-            else:
-              jsonFile.write(",\n  " + json.dumps(bukkenJson))
+        try:
+          while hasNextPage:
+            page+=1
+            bukkens = requestBuilder.postBukkenResult(block=block, tdfk=tdfk, skcs=skcs, page=page)
+            if (bukkens == None or len(bukkens) == 0 or int(bukkens[0]["roomCount"]) == 0):
+              break
+            for bukkenJson in bukkens:
+              jsonRowCount += 1
+              # write to json file
+              if (jsonRowCount == 0):
+                jsonFile.write("\n  " + json.dumps(bukkenJson))
+              else:
+                jsonFile.write(",\n  " + json.dumps(bukkenJson))
 
-            df = converter.toDf(converter.toBukken(bukkenJson))
-            # skip if df is empty
-            if df.empty:
-              continue
-            df.to_csv(
-              path_or_buf=csvFile,
-              header=bukkenFields.keys() if showHeader else False,
-              columns=bukkenFields.values(),
-              encoding="utf-8",
-              index=False,
-            )
-            showHeader = False
-          hasNextPage = len(bukkens) >= 0 or not isDev
-          # hasNextPage = False
+              df = converter.toDf(converter.toBukken(bukkenJson))
+              # skip if df is empty
+              if df.empty:
+                continue
+              df.to_csv(
+                path_or_buf=csvFile,
+                header=bukkenFields.keys() if showHeader else False,
+                columns=bukkenFields.values(),
+                encoding="utf-8",
+                index=False,
+              )
+              showHeader = False
+            hasNextPage = len(bukkens) >= 0 or not isDev
+        except Exception as e:
+          print(f"[error] on handling bukken {block} {tdfk} {skcs} {page}")
+          traceback.print_exc()
   jsonFile.write("\n]\n")
